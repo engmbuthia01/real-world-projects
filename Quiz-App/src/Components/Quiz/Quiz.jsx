e454e08900c512f50852a0f4d159460fd8e77066
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-10-19 22:11:32 -0400

    created the structure of the questions and choices and added styling to the Quiz component

---
+++ Quiz-App/src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Quiz.css";
+
+const Quiz = () => {
+    return (
+        <div className='container'>
+            <h1>Quiz App</h1>
+            <hr />
+            <h2>Which device is required for the internet connection?</h2>
+            <ul>
+                <li>Modem</li>
+                <li>Router</li>
+                <li>LAN cable</li>
+                <li>Pen Drive</li>
+            </ul>
+            <button>Next</button>
+            <div className="index">1 of 5 questions</div>
+        </div>
+    )
+}
+
+export default Quiz

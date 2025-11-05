f25e66678f2f582d2526fdf24335f0d05f667922
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-11-05 01:21:16 -0500

    ensured the skill component is responsive to different devices

--- portfolio/src/components/Skills/Skills.js
+++ portfolio/src/components/Skills/Skills.js
@@ -10,21 +10,21 @@ const Skills = () => {
       <span className='skillTitle'>What I do</span>
       <span className='skillDesc'>I am a skilled and passionate web designer with experience in creating visually appealing and user-friendly websites. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML, CSS, and JavaScript, as well as design software such as Adobe Photoshop and Illustrator.</span>
       <div className='skillBars'>
-        <div skillBar>
+        <div className='skillBar'>
           <img src={UIDesign} alt='UIDesign' className='skillBarImg' />
           <div className='skillBarText'>
             <h2>UI/UX Design</h2>
             <p>This is a demo text, you can write your own content here.</p>
           </div>
         </div>
-        <div skillBar>
+        <div className='skillBar'>
           <img src={WebDesign} alt='WebDesign' className='skillBarImg' />
           <div className='skillBarText'>
             <h2>Website Design</h2>
             <p>This demo text can be changed while making the production ready website.</p>
           </div>
         </div>
-        <div skillBar>
+        <div className='skillBar'>
           <img src={AppDesign} alt='AppDesign' className='skillBarImg' />
           <div className='skillBarText'>
             <h2>App Design</h2>

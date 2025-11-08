131a1d44118d376aaf51ca0db3affeda2073274c
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-11-08 05:42:44 -0500

    created and styled the Footer component

---
+++ portfolio/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        copyright &#169; 2025 Kevin Mbuthia. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer

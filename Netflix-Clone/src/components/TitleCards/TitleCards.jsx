f97fadf5284a158ff23378ffa5ae5b7027d06749
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-11-19 16:29:08 -0500

    mapped thumbnails unto the Home component

--- Netflix-Clone/src/components/TitleCards/TitleCards.jsx
+++ Netflix-Clone/src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import "./TitleCards.css";
+import cards_data from '../../assets/cards/Cards_data';
 
 const TitleCards = () => {
+  const cardsRef = useRef();
   return (
-    <div className='titlecards'>TitleCards</div>
+    <div className='title-cards'>
+      <h2>Popular on netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 

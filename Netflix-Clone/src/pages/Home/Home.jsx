408bb2a4236ea0fa67009f1fbbb555910fc610d8
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-11-19 07:25:06 -0500

    added the play and more info buttons

--- Netflix-Clone/src/pages/Home/Home.jsx
+++ Netflix-Clone/src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.css";
 import NavBar from '../../components/NavBar/NavBar';
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 
 const Home = () => {
   return (
@@ -16,6 +18,10 @@ const Home = () => {
             Discovering his ties to a secret ancient order, a young man living in modern Istanbul
             embarks on a quest to save the city from an immortal enemy
           </p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
         </div>
       </div>
     </div>

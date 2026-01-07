4982505956809b28420d4ecb574517dc7d5a396c
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-01-07 02:30:29 -0500

    embeded a video on the Playter componenet and mounted the component in App.jsx

--- Netflix-Clone/src/pages/Player/Player.jsx
+++ Netflix-Clone/src/pages/Player/Player.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
 
 const Player = () => {
   return (
-    <div className='player'>Player</div>
-  )
-}
+    <div className="player">
+      <img src={back_arrow_icon} alt="" />
+      <iframe
+        width="90%"
+        height="90%"
+        src="https://www.youtube.com/embed/jkAQBIG8bP8"
+        title="trailer"
+        frameborder="0"
+        allowFullScreeen
+      ></iframe>
+      <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
+    </div>
+  );
+};
 
-export default Player
+export default Player;

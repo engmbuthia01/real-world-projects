93d3661a39cfb3439523967464cd0f51613145ae
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-01-08 07:54:35 -0500

    created state variables for name, email and password

--- Netflix-Clone/src/pages/Login/Login.jsx
+++ Netflix-Clone/src/pages/Login/Login.jsx
@@ -1,18 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
+import { login, signup } from "../../firebase";
 
 const Login = () => {
-  const [signState, setSignState] = useState("Sign In")
+  const [signState, setSignState] = useState("Sign In");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div className='login'>
+    <div className="login">
       <img src={logo} alt="" className="login-logo" />
       <div className="login-form">
         <h1>{signState}</h1>
-        <form >
-          {signState === "Sign Up" ? <input type="text" placeholder='Your Name'/>: <></>}
-          <input type="email" placeholder='Email' />
-          <input type="password" placeholder='Password' />
+        <form>
+          {signState === "Sign Up" ? (
+            <input
+              value={name}
+              onChange={(e) => {
+                e.target.value;
+              }}
+              type="text"
+              placeholder="Your Name"
+            />
+          ) : (
+            <></>
+          )}
+          <input
+            value={email}
+            onChange={(e) => {
+              e.target.value;
+            }}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              e.target.value;
+            }}
+            type="password"
+            placeholder="Password"
+          />
           <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
@@ -23,11 +52,33 @@ const Login = () => {
           </div>
         </form>
         <div className="form-switch">
-          {signState==="Sign Up"?<p>Already have an account? <span onClick={() => {setSignState("Sign In")}}>Sign In Now</span></p>:<p>New to netflix? <span onClick={() => {setSignState("Sign Up")}}>Sign Up Now</span></p>}          
+          {signState === "Sign Up" ? (
+            <p>
+              Already have an account?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign In");
+                }}
+              >
+                Sign In Now
+              </span>
+            </p>
+          ) : (
+            <p>
+              New to netflix?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign Up");
+                }}
+              >
+                Sign Up Now
+              </span>
+            </p>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

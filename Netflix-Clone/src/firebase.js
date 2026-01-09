5ef511c1054e97af45557ecf00170bdc5d43ea42
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-01-09 02:32:23 -0500

    installed react-toastify for handling the error messages

--- Netflix-Clone/src/firebase.js
+++ Netflix-Clone/src/firebase.js
@@ -6,6 +6,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCR-NLiQqGUuv6auFlZoqlpGyu426xkUqY",
@@ -32,7 +33,7 @@ const signup = async (name, email, password) => {
     });
   } catch (error) {
     console.log(error);
-    alert(error);
+    toast.error(error.code);
   }
 };
 
@@ -41,7 +42,7 @@ const login = async (email, password) => {
     await signInWithEmailAndPassword(auth, email, password);
   } catch (error) {
     console.log(error);
-    alert(error);
+    toast.error(error.code);
   }
 };
 

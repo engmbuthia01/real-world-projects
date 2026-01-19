ed6634081a35aff8dcbd209d2b7bff966d5675d1
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-01-19 05:15:56 -0500

    created the sidebar with all images in it

--- Youtube-Clone/src/components/SideBar/SideBar.jsx
+++ Youtube-Clone/src/components/SideBar/SideBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./SideBar.css";
 import home from "../../assets/home.png";
 import game_icon from "../../assets/game_icon.png";
-import automibiles from "../../assets/automobiles.png";
+import automobiles from "../../assets/automobiles.png";
 import sports from "../../assets/sports.png";
 import entertainment from "../../assets/entertainment.png";
 import tech from "../../assets/tech.png";
@@ -16,7 +16,49 @@ import megan from "../../assets/megan.png";
 import cameron from "../../assets/cameron.png";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="sidebar">
+      <div className="shortcut-links">
+        <div className="side-links">
+          <img src={home} alt="" />
+          <p>Home</p>
+        </div>
+        <div className="side-links">
+          <img src={game_icon} alt="" />
+          <p>Gaming</p>
+        </div>
+        <div className="side-links">
+          <img src={automobiles} alt="" />
+          <p>Automobiles</p>
+        </div>
+        <div className="side-links">
+          <img src={sports} alt="" />
+          <p>Sports</p>
+        </div>
+        <div className="side-links">
+          <img src={entertainment} alt="" />
+          <p>Entertainment</p>
+        </div>
+        <div className="side-links">
+          <img src={tech} alt="" />
+          <p>Technology</p>
+        </div>
+        <div className="side-links">
+          <img src={music} alt="" />
+          <p>Music</p>
+        </div>
+        <div className="side-links">
+          <img src={blogs} alt="" />
+          <p>Blogs</p>
+        </div>
+        <div className="side-links">
+          <img src={news} alt="" />
+          <p>News</p>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;

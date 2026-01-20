777cdf062f8859b3650a09b6ac91b867a3e8b384
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-01-20 02:09:30 -0500

    styled the Home component's container and large-container class names

--- Youtube-Clone/src/pages/Home/Home.jsx
+++ Youtube-Clone/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Home.css";
 import SideBar from "../../components/SideBar/SideBar";
+import Feed from "../../components/Feed/Feed";
 
 const Home = ({ sidebar }) => {
   return (
     <>
       <SideBar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large - container"}`}>
+        <Feed />
+      </div>
     </>
   );
 };

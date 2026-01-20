179c0b0671f6fc7ba2e2275ddd753a9e09a13734
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-01-20 00:07:40 -0500

    passed values from the parent component to the child component

--- Youtube-Clone/src/App.jsx
+++ Youtube-Clone/src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Video from "./pages/Video/Video";
 
 const App = () => {
+  const [sidebar, setSidebar] = useState(true);
   return (
     <div>
-      <NavBar />
+      <NavBar setSidebar={setSidebar} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/video/:ctegoryId/:videoId" element={<Video />} />
+        <Route path="/" element={<Home sidebar={sidebar} />} />
+        <Route path="/video/:categoryId/:videoId" element={<Video />} />
       </Routes>
     </div>
   );

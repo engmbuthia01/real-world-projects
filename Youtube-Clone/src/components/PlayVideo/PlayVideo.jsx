c15163769a907b6cc97eedd872c5d3b88d195924
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-02-02 00:28:28 -0500

    added the autoplay functionality

--- Youtube-Clone/src/components/PlayVideo/PlayVideo.jsx
+++ Youtube-Clone/src/components/PlayVideo/PlayVideo.jsx
@@ -13,7 +13,7 @@ const PlayVideo = ({ videoId }) => {
     <div className="play-video">
       {/* <video src={video1} controls autoPlay muted></video> */}
       <iframe
-        src={`https://www.youtube.com/embed/${videoId}`}
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"

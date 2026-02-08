f6e17238d8a92c4c7b00dad0846f7442cb112e2c
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-02-08 11:33:42 -0500

    installed link from react router dom

--- Youtube-Clone/src/components/NavBar/NavBar.jsx
+++ Youtube-Clone/src/components/NavBar/NavBar.jsx
@@ -7,7 +7,6 @@ import upload_icon from "../../assets/upload.png";
 import more_icon from "../../assets/more.png";
 import notification_icon from "../../assets/notification.png";
 import profile_icon from "../../assets/jack.png";
-import { Link } from "react-router-dom";
 
 const NavBar = ({ setSidebar }) => {
   return (
@@ -19,9 +18,7 @@ const NavBar = ({ setSidebar }) => {
           src={menu_icon}
           alt=""
         />
-        <Link to="/">
-          <img className="logo" src={logo} alt="" />
-        </Link>
+        <img className="logo" src={logo} alt="" />
       </div>
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">

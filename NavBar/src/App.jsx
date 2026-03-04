ff0dd6dd3a94b26fe87faaf8e01e09e510706f09
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-03-04 12:33:28 -0500

    added the useEffect hook to handle the changes in the theme

--- NavBar/src/App.jsx
+++ NavBar/src/App.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   const current_theme = localStorage.getItem("current_theme");
   const [theme, setTheme] = useState(current_theme ? current_theme : "light");
+  useEffect(() => {
+    localStorage.setItem("current_theme", theme);
+  }, [theme]);
   return (
     <div className={`container ${theme}`}>
       <NavBar theme={theme} setTheme={setTheme} />

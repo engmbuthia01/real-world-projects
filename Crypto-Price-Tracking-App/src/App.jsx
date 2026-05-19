e7e3d1674f7e8f6f4c00b8ae87eaee27844c5b76
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-05-19 06:01:50 -0400

    added routing to the Home and Coin pages

--- Crypto-Price-Tracking-App/src/App.jsx
+++ Crypto-Price-Tracking-App/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Coin from "./pages/Coin/Coin";
 
 const App = () => {
   return (
     <div className="app">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin/:coinId" element={<Coin />} />
+      </Routes>
     </div>
   );
 };

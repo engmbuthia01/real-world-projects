48b140adf7a172ce5602b665337e174016b61a5d
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-05-19 03:28:27 -0400

    created the NavBar component and mounted it to the parent component

---
+++ Crypto-Price-Tracking-App/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NavBar.css";
+import logo from "../../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" />
+    </div>
+  );
+};
+
+export default NavBar;

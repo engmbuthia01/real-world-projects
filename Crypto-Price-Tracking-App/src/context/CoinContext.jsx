c0b9f84740e93cf12ef81805c4f73e44c332505b
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-05-21 07:23:34 -0400

    created a context provider function

--- Crypto-Price-Tracking-App/src/context/CoinContext.jsx
+++ Crypto-Price-Tracking-App/src/context/CoinContext.jsx
@@ -1,3 +1,12 @@
 import { createContext } from "react";
 
 export const CoinContext = createContext();
+
+const CoinContextProvider = (props) => {
+    return (
+        <CoinContext.Provider value={}>
+            {props.children}
+        </CoinContext.Provider>
+    )
+
+}

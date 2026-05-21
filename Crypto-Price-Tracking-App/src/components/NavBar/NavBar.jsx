72ec7d04d968e68a02e13d40bfc9ad5d6253f44c
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-05-21 09:41:02 -0400

    added a currencyHandler function to handle the change in currencies

--- Crypto-Price-Tracking-App/src/components/NavBar/NavBar.jsx
+++ Crypto-Price-Tracking-App/src/components/NavBar/NavBar.jsx
@@ -2,9 +2,31 @@ import React, { useContext } from "react";
 import "./NavBar.css";
 import logo from "../../assets/logo.png";
 import arrow_icon from "../../assets/arrow_icon.png";
+import { CoinContext } from "../../context/CoinContext";
 
 const NavBar = () => {
   const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: "eur", symbol: "₹" });
+        break;
+      }
+      default: {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+    }
+  };
   return (
     <div className="navbar">
       <img src={logo} alt="" className="logo" />
@@ -15,7 +37,7 @@ const NavBar = () => {
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="euro">EURO</option>
           <option value="inr">INR</option>

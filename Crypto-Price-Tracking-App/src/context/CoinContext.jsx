e6c275ccfaf5fbae4a611cf6d9902f585157443a
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-05-21 07:34:09 -0400

    wrapped the App component inside the CoinContextProvider

--- Crypto-Price-Tracking-App/src/context/CoinContext.jsx
+++ Crypto-Price-Tracking-App/src/context/CoinContext.jsx
@@ -3,10 +3,13 @@ import { createContext } from "react";
 export const CoinContext = createContext();
 
 const CoinContextProvider = (props) => {
-    return (
-        <CoinContext.Provider value={}>
-            {props.children}
-        </CoinContext.Provider>
-    )
+  const contextValue = {};
 
-}
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+};
+
+export default CoinContextProvider;

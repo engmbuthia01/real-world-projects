d95472049b0da32b49b8e414c31502ba39d4b79a
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-05-21 23:10:42 -0400

    displayed the coins numbering

--- Crypto-Price-Tracking-App/src/pages/Home/Home.jsx
+++ Crypto-Price-Tracking-App/src/pages/Home/Home.jsx
@@ -32,6 +32,11 @@ const Home = () => {
           <p style={{ textAlign: "center" }}>24H Change</p>
           <p className="market-cap">Market Cap</p>
         </div>
+        {displayCoin.slice(0, 10).map((item, index) => (
+          <div className="table-layout" key={index}>
+            <p>{item.market_cap_rank}</p>
+          </div>
+        ))}
       </div>
     </div>
   );

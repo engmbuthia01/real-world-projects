ceff1bf43bb664e5695f85f63266254c84d16559
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-05-24 04:42:14 -0400

    added a state variable to store coin data and a function to fetch the data from the coingecko API

--- Crypto-Price-Tracking-App/src/pages/Coin/Coin.jsx
+++ Crypto-Price-Tracking-App/src/pages/Coin/Coin.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Coin.css";
+import { useParams } from "react-router-dom";
 
 const Coin = () => {
-  return <div>Coin</div>;
+  const { coinId } = useParams();
+
+  const [coinData, setCoinData] = useState();
+
+  const fetchCoinData = (async) => {
+    const options = {
+      method: "GET",
+      headers: { "x-cg-demo-api-key": "CG-F6g5Yy3HThVYLqEtwbsqcqXF" },
+    };
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
+  };
+
+  return <div>Coin : {coinId}</div>;
 };
 
 export default Coin;

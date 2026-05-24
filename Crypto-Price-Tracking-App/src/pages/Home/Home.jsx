c1b1a7d461b16815470e324d4e6b6f716eb52d69
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-05-24 02:53:14 -0400

    replaced div with Link in the table-layout to enable routing

--- Crypto-Price-Tracking-App/src/pages/Home/Home.jsx
+++ Crypto-Price-Tracking-App/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import "./Home.css";
 import { CoinContext } from "../../context/CoinContext";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const { allCoin, currency } = useContext(CoinContext);
@@ -64,7 +65,7 @@ const Home = () => {
           <p className="market-cap">Market Cap</p>
         </div>
         {displayCoin.slice(0, 10).map((item, index) => (
-          <div className="table-layout" key={index}>
+          <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
             <p>{item.market_cap_rank}</p>
             <div>
               <img src={item.image} alt="" />
@@ -81,7 +82,7 @@ const Home = () => {
             <p className="market-cap">
               {currency.symbol} {item.market_cap.toLocaleString()}
             </p>
-          </div>
+          </Link>
         ))}
       </div>
     </div>

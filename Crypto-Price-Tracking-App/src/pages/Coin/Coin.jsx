7165bdf893185d02b93a3ba2539f1d208cf12861
engmbuthia01 <kevinbmbuthia3354@gmail.com>
2026-07-03 20:32:12 +0300

    updated the if condition

--- Crypto-Price-Tracking-App/src/pages/Coin/Coin.jsx
+++ Crypto-Price-Tracking-App/src/pages/Coin/Coin.jsx
@@ -28,7 +28,7 @@ const Coin = () => {
     };
 
     fetch(
-      "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=10",
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10`,
       options,
     )
       .then((res) => res.json())

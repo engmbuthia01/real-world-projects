509c198e22d61f6b79ca476569711e4e3ddc0919
engmbuthia01 <kevinbmbuthia3354@gmail.com>
2026-07-07 20:26:10 +0300

    added styling to the Programs component

--- educity/src/components/Programs/Programs.jsx
+++ educity/src/components/Programs/Programs.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Programs.css";
+import Program_1 from "../../assets/program-1.png";
+import Program_2 from "../../assets/program-2.png";
+import Program_3 from "../../assets/program-3.png";
 
 const Programs = () => {
-  return <div></div>;
+  return (
+    <div className="programs">
+      <div className="program">
+        <img src={Program_1} alt="" />
+      </div>
+      <div className="program">
+        <img src={Program_2} alt="" />
+      </div>
+      <div className="program">
+        <img src={Program_3} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Programs;

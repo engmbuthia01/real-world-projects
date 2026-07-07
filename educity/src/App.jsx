1cb58a6fadfa13e4d2fd0ef7618d54747ee2a169
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-07-07 22:30:49 +0300

    passed the subTitle and title as props to the title component

--- educity/src/App.jsx
+++ educity/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
+import Title from "./components/Title/Title";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      <div className="container">
+        <Title subTitle="OUR PROGRAMS" title="What We Offer" />
+        <Programs />
+      </div>
     </div>
   );
 };

195c253d9ac141f105b840656ce53c2a8c563bdb
engmbuthia01 <kevinbmbuthia3354@gmail.com>
2026-07-07 10:27:19 +0300

    added styling to the NavBar component

--- educity/src/components/NavBar/NavBar.jsx
+++ educity/src/components/NavBar/NavBar.jsx
@@ -4,15 +4,17 @@ import logo from "../../assets/logo.png";
 
 const NavBar = () => {
   return (
-    <nav>
-      <img src={logo} alt="" />
+    <nav className="container">
+      <img src={logo} alt="" className="logo" />
       <ul>
         <li>Home</li>
         <li>Program</li>
         <li>About us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li>Contact us</li>
+        <li>
+          <button className="btn">Contact us</button>
+        </li>
       </ul>
     </nav>
   );

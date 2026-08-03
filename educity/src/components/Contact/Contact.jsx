c9af5a30411084470a65b41c23e1667099a2fe49
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-08-03 13:37:34 -0400

    created a contact component and added the left column elements

--- educity/src/components/Contact/Contact.jsx
+++ educity/src/components/Contact/Contact.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import "./Contact.css";
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/location-icon.png";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message <img src={msg_icon} alt="" />
+        </h3>
+        <p>
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community
+        </p>
+        <ul>
+          <li>
+            <img src={mail_icon} alt="" />
+            contact@educity.com
+          </li>
+          <li>
+            <img src={phone_icon} alt="" />
+            +1 784-239-3645
+          </li>
+          <li>
+            <img src={location_icon} alt="" />
+            4633 Raccoon Run, Seattle, WA 98133
+          </li>
+        </ul>
+      </div>
+      <div className="contact-col"></div>
+    </div>
+  );
 };
 
 export default Contact;

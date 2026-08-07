e55ae21128437f6ea273fc3c2dd1dd85e1755be8
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-08-07 10:35:16 -0400

    added the boilerplate for the form and styled it

--- educity/src/components/Contact/Contact.jsx
+++ educity/src/components/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import msg_icon from "../../assets/msg-icon.png";
 import mail_icon from "../../assets/mail-icon.png";
 import phone_icon from "../../assets/phone-icon.png";
 import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
 
 const Contact = () => {
   return (
@@ -33,7 +34,35 @@ const Contact = () => {
           </li>
         </ul>
       </div>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <form>
+          <label>Your name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your phone number"
+            required
+          />
+          <label>Your message here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit now <img src={white_arrow} alt="" />
+          </button>
+        </form>
+        <span></span>
+      </div>
     </div>
   );
 };

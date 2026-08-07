db3a1c85dd9b37ca0886e725b5feb8abb99b8994
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-08-07 16:00:40 -0400

    populated and styled the Footer

--- educity/src/components/Footer/Footer.jsx
+++ educity/src/components/Footer/Footer.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <p>@ 2024 Educity. All rights reserved</p>
+      <ul>
+        <li>Terms of Services</li>
+        <li>Privacy Policy</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;

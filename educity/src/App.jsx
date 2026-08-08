c675d66d757f899adc8305718a2d8ff03fc8f82b
engmbuthia01 <kevinmbuthia3354@gmail.com>
2026-08-08 02:16:31 -0400

    added the react scroll feature to enable smooth scrolling between the components

--- educity/src/App.jsx
+++ educity/src/App.jsx
@@ -6,6 +6,8 @@ import Title from "./components/Title/Title";
 import About from "./components/About/About";
 import Campus from "./components/Campus/Campus";
 import Testimonials from "./components/Testimonials/Testimonials";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -21,6 +23,8 @@ const App = () => {
         <Title subTitle="Testimonials" title="What Students Say" />
         <Testimonials />
         <Title subTitle="Contact Us" title="Get in Touch" />
+        <Contact />
+        <Footer />
       </div>
     </div>
   );
